be86d6421930b77834c6735977c737aed360bed3
Guzman <cguzman65@cps.k12.il.us>
2024-09-03 12:28:59 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+#comments are used to help understand
+name= input ("What is your name?")
+print ("Hello", name)
+# this is what input does 
+#ask the user for their age 
+age = input ("How old are you?")
+#print the users age 
+print ("You are", age,"years old" )
+#ask the user for their favorite food
+color= input ("What is your favorite color?")
+#print out favorite food
+print("your favorite color is",color )
